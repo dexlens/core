405912004b8e903660003103dd401d5acfb072f2
liquid <13621351+liqtags@users.noreply.github.com>
2024-11-19 17:27:13 +0000

    Add formatValueToCurrency

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { logger } from "./packages/logger/index.ts";
-
+import { formatValueToCurrency } from "./packages/formatValueToCurrency/index.ts";
 export {
-    logger
+    logger,
+    formatValueToCurrency
 }
