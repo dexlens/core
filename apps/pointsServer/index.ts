ba158a50521df6a67a6a51a806296a9c2a8987b8
liquid <13621351+liqtags@users.noreply.github.com>
2024-11-27 04:44:46 +0000

    code for /start command

--- apps/pointsServer/index.ts
+++ apps/pointsServer/index.ts
@@ -4,12 +4,13 @@ import dotenv from "npm:dotenv";
 dotenv.config();
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const DEXL_CHANNEL_ID = process.env.DEXL_CHANNEL_ID as string;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 const bot = new Bot(process.env.DEV_BOT_API_TOKEN);
 import { useStartCommand } from "./commands/start.ts";
 
-/**
- * Add your bot logic here
- */
+useStartCommand(bot, supabase, {
+    DEXL_CHANNEL_ID,
+});
 
 bot.start();
